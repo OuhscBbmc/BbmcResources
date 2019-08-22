777f5abe562f9187e69cf0c1e2cec59fee548b44
Will Beasley <wibeasley@hotmail.com>
2019-08-21 23:55:32 -0500

    shorter logo

--- Diagrams/BbmcLogos/BbmcDistribution.R
+++ Diagrams/BbmcLogos/BbmcDistribution.R
@@ -9,8 +9,8 @@ lineSize <- 1
 interpolationPoints <- 1001
 
 g <- ggplot(data.frame(x=seq(from=0, to=10, length=5)), aes(x)) +
-  geom_vline(x=0, color=color2) +
-  geom_hline(y=0, color=color2) +
+  geom_vline(xintercept=0, color=color2) +
+  geom_hline(yintercept=0, color=color2) +
   stat_function(fun = dchisq, args=list(df=1), colour=color2, size=lineSize, n=interpolationPoints) +
   stat_function(fun = dchisq, args=list(df=2), colour=color2, size=lineSize, n=interpolationPoints) +
   stat_function(fun = dchisq, args=list(df=3), colour=color1, size=lineSize, n=interpolationPoints) +
@@ -24,8 +24,8 @@ g <- ggplot(data.frame(x=seq(from=0, to=10, length=5)), aes(x)) +
   scale_y_continuous(expand = c(0, 0), limits=c(0, .6)) +
   labs(x="Biomedical and Behavioral Methodology CORE", y=NULL, main=NULL) +
 #   annotate(geom="text", x=1, y=.4, label="BBMC", hjust=0, size=28, color=color2, fontface="bold") +
-  annotate(geom="text", x=2.3, y=Inf, label="beta*beta*Mu*italic(C)", parse=TRUE, hjust=0, vjust=1, size=34, color=color1, fontface="bold") +  
-  
+  annotate(geom="text", x=2.3, y=Inf, label="beta*beta*Mu*italic(C)", parse=TRUE, hjust=0, vjust=1, size=34, color=color1, fontface="bold") +
+
   theme_bw() +
   theme(panel.grid=element_blank()) +
   theme(panel.border=element_blank()) +
@@ -34,7 +34,7 @@ g <- ggplot(data.frame(x=seq(from=0, to=10, length=5)), aes(x)) +
   theme(axis.title=element_text(face="italic", vjust=1)) +
   theme(plot.background=element_blank()) +
   theme(panel.background=element_blank()) +
-  theme(axis.ticks=element_line(colour=color1)) + 
+  theme(axis.ticks=element_line(colour=color1)) +
   theme(plot.margin = unit(c(0,0,0,0), "cm"))
 
 ggsave(plot=g, filename="./Diagrams/BbmcLogos/BbmcDistributionsBackgroundClear.png", width=5.5, height=2.2, dpi=1200, bg=NA)
